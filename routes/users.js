a0b1634b334f20e92c2c7289e214ccb267891730
nOSkr <122243978+nOSkr04@users.noreply.github.com>
2023-10-30 03:20:11 +0800

    sync

---
+++ routes/users.js
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import { protect, authorize } from "../middleware/protect.js";
+
+import {
+  register,
+  login,
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  logout,
+  authMeUser,
+  invoiceTime,
+  chargeTime,
+  invoiceCheck,
+} from "../controller/users.js";
+
+const router = Router();
+
+//"/api/v1/users"
+router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/logout").get(logout);
+// router.route("/updatePrivacy").post(updatePrivacy);
+
+router.route("/callbacks/:id/:numId").get(chargeTime);
+router.route("/check/challbacks/:id/:numId").get(invoiceCheck);
+router.use(protect);
+
+//"/api/v1/users"
+router
+  .route("/")
+  .get(authorize("admin"), getUsers)
+  .post(authorize("admin"), createUser);
+router.route("/me").get(protect, authMeUser);
+router.route("/invoice/:id").post(invoiceTime);
+router.route("/:id").get(getUser).put(updateUser).delete(protect, deleteUser);
+
+export default router;
