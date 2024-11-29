6b8f2a04c17a3743de71af40bfeafecfd377a8db
nOSkr <tselmen.ihelp@gmail.com>
2024-11-29 12:50:52 +0800

    sync

--- server.js
+++ server.js
@@ -53,13 +53,19 @@ var whitelist = [
 var corsOptions = {
   // Ямар ямар домэйнээс манай рест апиг дуудаж болохыг заана
   origin: function (origin, callback) {
-    if (whitelist.includes(origin)) {
-      return callback(null, true);
+    if (origin === undefined || whitelist.indexOf(origin) !== -1) {
+      // Энэ домэйнээс манай рест рүү хандахыг зөвшөөрнө
+      callback(null, true);
     } else {
-      console.log("BLOCKED origin:", origin);
-      return callback(new Error("Horigloj baina.."), false);
+      // Энэ домэйнд хандахыг хориглоно.
+      callback(new Error("Horigloj baina.."));
     }
   },
+  // Клиент талаас эдгээр http header-үүдийг бичиж илгээхийг зөвшөөрнө
+  allowedHeaders: "Authorization, Set-Cookie, Content-Type",
+  // Клиент талаас эдгээр мэссэжүүдийг илгээхийг зөвөөрнө
+  methods: "GET, POST, PUT, DELETE",
+  // Клиент тал authorization юмуу cookie мэдээллүүдээ илгээхийг зөвшөөрнө
   credentials: true,
 };
 
